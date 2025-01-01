b203955b8833c4b1621c0d8d5491e5b1ea86e0cb
Maverick Wong <mvrck.wong@gmail.com>
2025-01-01 08:57:45 +0800

    Update

--- models/silver/stg_seed_stores.sql
+++ models/silver/stg_seed_stores.sql
@@ -7,7 +7,19 @@
 
 WITH source AS (
       SELECT
-		*
+		store_id
+		, region_id
+		, store_type
+		, store_name
+		, store_street_address
+		, store_city
+		, store_state
+		, store_country
+		, store_phone
+		, first_opened_date AS store_first_opened_date
+		, last_remodel_date AS store_last_remodel_date
+		, total_sqft AS store_total_sqft
+		, grocery_sqft AS store_grocery_sqft
       FROM 
             {{ ref('raw_seed_stores') }}
 )
