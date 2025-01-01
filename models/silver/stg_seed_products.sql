6d21e99c285880b7a62d31d82eb4bc6497a63a95
Maverick Wong <mvrck.wong@gmail.com>
2025-01-01 08:37:55 +0800

    Fix product dimension table

--- models/silver/stg_seed_products.sql
+++ models/silver/stg_seed_products.sql
@@ -7,9 +7,19 @@
 
 WITH source AS (
       SELECT
-		*
+		product_id
+		, product_brand
+		, product_name
+		, product_sku
+		, product_retail_price
+		, product_cost
+		, product_weight
+		, recyclable AS is_product_recyclable
+		, low_fat AS is_product_low_fat
       FROM 
             {{ ref('raw_seed_products') }}
+	ORDER BY
+		product_id DESC
 )
 SELECT
 	*
