9516589f3b9bd48f2bf2f2f3469698ccbf2e832f
Maverick Wong <mvrck.wong@gmail.com>
2025-04-03 11:42:57 +0800

    Add standard metrics

--- models/marts/eg-adworks/agg_returns.sql
+++ models/marts/eg-adworks/agg_returns.sql
@@ -3,27 +3,60 @@
 		materialized = 'view',
 		tags=[
 			'adworks',
-			'agg'
+			'metrics'
 		]
 	)
 }}
 
-WITH agg_stats AS (
+WITH return_metrics AS (
 	SELECT
-		count(return_date) AS count_returns,
-		sum(return_quantity) AS sum_return_quantity
-	FROM 
+		-- Metric 1: Total return count
+		count(return_date) AS total_return_count,
+		
+		-- Metric 2: Total return quantity
+		sum(return_quantity) AS total_return_quantity,
+		
+		-- Metric 3: Average return quantity per return
+		CASE 
+			WHEN count(return_date) > 0 THEN sum(return_quantity) / count(return_date)
+			ELSE 0 
+		END AS avg_quantity_per_return,
+		
+		-- Metric 4: Count of returns by territory
+		count(distinct territory_key) AS territory_return_count,
+		
+		-- Metric 5: Count of products returned
+		count(distinct product_key) AS product_return_count
+  	FROM 
 		{{ ref('fact_returns_adworks') }}
 )
 
 SELECT
-	'count_returns' AS name, 
-	count_returns AS attribute
+	'total_return_count' AS name, 
+	total_return_count AS attribute
 FROM 
-	agg_stats
+  	return_metrics
 UNION ALL
 SELECT 
-	'sum_return_quantity' AS name, 
-	sum_return_quantity AS attribute 
+	'total_return_quantity' AS name, 
+	total_return_quantity AS attribute 
 FROM 
-	agg_stats
+  	return_metrics
+UNION ALL
+SELECT 
+	'avg_quantity_per_return' AS name, 
+	avg_quantity_per_return AS attribute 
+FROM 
+  	return_metrics
+UNION ALL
+SELECT 
+	'territory_return_count' AS name, 
+	territory_return_count AS attribute 
+FROM 
+  	return_metrics
+UNION ALL
+SELECT 
+	'product_return_count' AS name, 
+	product_return_count AS attribute 
+FROM 
+	return_metrics
