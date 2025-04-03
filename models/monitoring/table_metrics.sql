5151fa317b4241539032d35c9749650ebef1fcc0
Maverick Wong <mvrck.wong@gmail.com>
2025-04-03 14:11:46 +0800

    Update table_metrics.sql

--- models/monitoring/table_metrics.sql
+++ models/monitoring/table_metrics.sql
@@ -6,10 +6,11 @@
 }}
 
 SELECT
-    table_schema || '.' || table_name AS table_path,
-    SUM(total_logical_bytes)/POWER(1024, 3) AS total_gb,
-    MAX(creation_time) AS created_at
+	table_schema || '.' || table_name AS table_path,
+	SUM(total_logical_bytes)/POWER(1024, 3) AS total_gb,
+	MAX(creation_time) AS created_at
 FROM
-    `{{ target.project }}.region-us-central1.INFORMATION_SCHEMA.TABLE_STORAGE`
+    	`{{ target.project }}.region-us-central1.INFORMATION_SCHEMA.TABLE_STORAGE`
 GROUP BY 1
-ORDER BY total_gb DESC
+ORDER BY 
+	total_gb DESC
