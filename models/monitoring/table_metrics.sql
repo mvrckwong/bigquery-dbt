a2d126468a7691e22fe8c16c161f8b32cce22db9
Maverick Wong <mvrck.wong@gmail.com>
2025-04-03 14:07:46 +0800

    Update

---
+++ models/monitoring/table_metrics.sql
@@ -0,0 +1,15 @@
+{{ 
+    config(
+        materialized='table',
+        enabled=false
+    )
+}}
+
+SELECT
+    table_schema || '.' || table_name AS table_path,
+    SUM(total_logical_bytes)/POWER(1024, 3) AS total_gb,
+    MAX(creation_time) AS created_at
+FROM
+    `{{ target.project }}.region-us-central1.INFORMATION_SCHEMA.TABLE_STORAGE`
+GROUP BY 1
+ORDER BY total_gb DESC
--- models/monitoring/user_metrics.sql
+++ models/monitoring/user_metrics.sql
@@ -1,16 +1,21 @@
--- models/monitoring/user_metrics.sql
-{{ config(materialized='table') }}
+
+{{ 
+	config(
+		materialized='table',
+		enabled=false
+	)
+}}
 
 SELECT
-  user_email,
-  COUNT(*) AS job_count,
-  SUM(total_bytes_processed)/POWER(1024, 4) AS total_tb_processed,
-  SUM(total_slot_ms)/(1000*60*60) AS total_slot_hours
+	user_email,
+	COUNT(*) AS job_count,
+	SUM(total_bytes_processed)/POWER(1024, 4) AS total_tb_processed,
+	SUM(total_slot_ms)/(1000*60*60) AS total_slot_hours
 FROM
-  `region-us-central1`.INFORMATION_SCHEMA.JOBS
+	`region-us-central1`.INFORMATION_SCHEMA.JOBS
 WHERE
-  creation_time > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 7 DAY)
+	creation_time > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 7 DAY)
 GROUP BY
-  user_email
+	user_email
 ORDER BY
-  total_slot_hours DESC
+	total_slot_hours DESC
